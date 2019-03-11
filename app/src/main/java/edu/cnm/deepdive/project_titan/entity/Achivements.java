fde084a522b93341b0390d0927c89694ee22a317
Lance Zotigh <lancezotighlz@gmail.com>
2019-03-11 14:40:41 -0600

    Added entities to our project

---
+++ app/src/main/java/edu/cnm/deepdive/project_titan/entity/Achivements.java
@@ -0,0 +1,91 @@
+package edu.cnm.deepdive.project_titan.entity;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(
+    foreignKeys = @ForeignKey(
+        entity = UserInfo.class,
+        parentColumns = "user_id", childColumns = "user_id",
+        onDelete = ForeignKey.CASCADE
+    )
+)
+public class Achivements {
+
+  @ColumnInfo(name = "achievements_id")
+  @PrimaryKey(autoGenerate = true)
+  private long id;
+
+  @ColumnInfo(name = "user_id")
+  private long userId;
+
+  @NonNull
+  @ColumnInfo(name = "assignment_name")
+  private String AssignmentName;
+
+  @ColumnInfo(name = "points_earned")
+  private long pointsEarned;
+
+
+  @NonNull
+  @ColumnInfo(name = "source_name")
+  private String sourceName;
+
+  @NonNull
+  @ColumnInfo(name = "achievement_origin")
+  private String achievementOrigin;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public long getUserId() {
+    return userId;
+  }
+
+  public void setUserId(long userId) {
+    this.userId = userId;
+  }
+
+  @NonNull
+  public String getAssignmentName() {
+    return AssignmentName;
+  }
+
+  public void setAssignmentName(@NonNull String assignmentName) {
+    AssignmentName = assignmentName;
+  }
+
+  public long getPointsEarned() {
+    return pointsEarned;
+  }
+
+  public void setPointsEarned(long pointsEarned) {
+    this.pointsEarned = pointsEarned;
+  }
+
+  @NonNull
+  public String getSourceName() {
+    return sourceName;
+  }
+
+  public void setSourceName(@NonNull String sourceName) {
+    this.sourceName = sourceName;
+  }
+
+  @NonNull
+  public String getAchievementOrigin() {
+    return achievementOrigin;
+  }
+
+  public void setAchievementOrigin(@NonNull String achievementOrigin) {
+    this.achievementOrigin = achievementOrigin;
+  }
+}
---
+++ app/src/main/java/edu/cnm/deepdive/project_titan/entity/UserInfo.java
@@ -0,0 +1,71 @@
+package edu.cnm.deepdive.project_titan.entity;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Entity
+public class UserInfo {
+
+  @ColumnInfo(name = "user_id")
+  @PrimaryKey(autoGenerate =  true)
+  private long id;
+
+  @Expose
+  @SerializedName("user_name")
+  private String userName;
+
+  @Expose
+  @SerializedName("first_name")
+  private String FN;
+
+  @Expose
+  @SerializedName("last_name")
+  private String LN;
+
+  @Expose
+  @SerializedName("a_r_image")
+  private String ARImage;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getFN() {
+    return FN;
+  }
+
+  public void setFN(String FN) {
+    this.FN = FN;
+  }
+
+  public String getLN() {
+    return LN;
+  }
+
+  public void setLN(String LN) {
+    this.LN = LN;
+  }
+
+  public String getARImage() {
+    return ARImage;
+  }
+
+  public void setARImage(String ARImage) {
+    this.ARImage = ARImage;
+  }
+}
