053c52634c11b04b306c3e0570725888759f6a3a
Thomas Herrera <rayherrera90@outlook.com>
2019-04-05 10:18:43 -0600

    Ninja Fragment documentation.

--- app/src/main/java/edu/cnm/deepdive/project_titan/fragments/NinjaFragment.java
+++ app/src/main/java/edu/cnm/deepdive/project_titan/fragments/NinjaFragment.java
@@ -1,3 +1,16 @@
+/**
+ *  Copyright 2019 Lance Zotigh, Alex Rauenzahn, Thomas Herrera & Deep Dive Coding Licensed under the
+ *  Apache License, Version 2.0 (the “License”); you may not use this file except in compliance with the
+ *  License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package edu.cnm.deepdive.project_titan.fragments;
 
 import android.os.Bundle;
@@ -18,6 +31,12 @@ import edu.cnm.deepdive.project_titan.R;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.ux.TransformableNode;
 
+/**
+ * Creates an {@link ArFragment} within {@link NinjaFragment}. This fragment will ask the user for the proper
+ * permissions and then launches an augmented reality view that displays the ninja drop mesh. Users are able to
+ * interact with the mesh through simple gesture controls that will allow resizing, rotating, and creation of the
+ * mesh.
+ */
 
 public class NinjaFragment extends Fragment {
 
@@ -66,11 +85,4 @@ public class NinjaFragment extends Fragment {
         });
     return view;
   }
-
-//  @Override
-//  public void onResume() {
-//    super.onResume();
-//
-//
-//  }
 }
