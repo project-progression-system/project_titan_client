23f883bc9c07d1df5a1879981ca659d0c2b44ee2
Alex Rauenzahn <xandermv@yahoo.com>
2019-04-09 13:43:16 -0600

    Added Javadocs.

--- app/src/main/java/edu/cnm/deepdive/project_titan/fragments/NinjaFragment.java
+++ app/src/main/java/edu/cnm/deepdive/project_titan/fragments/NinjaFragment.java
@@ -1,6 +1,6 @@
 /**
  * Copyright 2019 Lance Zotigh, Alex Rauenzahn, Thomas Herrera & Deep Dive Coding Licensed under the
- * Apache License, Version 2.0 (the “License”); you may not use this file except in compliance with
+ * Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
