cec841746eb0090b8f0edda5d0f18494cd6ec953
Lance Zotigh <lancezotighlz@gmail.com>
2019-04-09 14:53:17 -0600

    added some java documentationII

--- app/src/main/java/edu/cnm/deepdive/project_titan/model/entity/Assignment.java
+++ app/src/main/java/edu/cnm/deepdive/project_titan/model/entity/Assignment.java
@@ -7,10 +7,20 @@ public class Assignment {
   @Expose
   private String name;
 
+  /**
+   * Gets the string name
+   *
+   * @return name
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * sets the name in a String
+   *
+   * @param name sets name
+   */
   public void setName(String name) {
     this.name = name;
   }
